3bccb6b2f06792f78722a28c6e23b99fa6f07ab4
Philippe Marschall <philippe.marschall@gmail.com>
2020-06-14 20:57:03 +0200

    struct metadata

--- src/test/java/com/github/marschall/aqapi/demo/CustomDataFactory.java
+++ src/test/java/com/github/marschall/aqapi/demo/CustomDataFactory.java
@@ -1,9 +1,14 @@
 package com.github.marschall.aqapi.demo;
 
 import java.sql.Connection;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Struct;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import oracle.jdbc.OracleStruct;
+import oracle.jdbc.OracleTypeMetaData;
 import oracle.sql.Datum;
 import oracle.sql.ORAData;
 import oracle.sql.ORADataFactory;
@@ -18,6 +23,29 @@ final class CustomDataFactory implements ORADataFactory {
   public ORAData create(Datum datum, int sqlType) throws SQLException {
     if (datum instanceof Struct) {
       Struct struct = (Struct) datum;
+      String sqlTypeName = struct.getSQLTypeName();
+      Object[] attributes = struct.getAttributes();
+      Map<String, Class<?>> typeMap = new LinkedHashMap<>();
+      typeMap.put("title", String.class);
+      typeMap.put("text", String.class);
+      typeMap.put("id", Long.class); // BigDecimal
+      Object[] orderedAttributes = struct.getAttributes(typeMap);
+      System.out.println(sqlTypeName);
+    }
+
+    if (datum instanceof OracleStruct) {
+      OracleStruct oracleStruct = (OracleStruct) datum;
+      OracleTypeMetaData oracleMetaData = oracleStruct.getOracleMetaData();
+      if (oracleMetaData instanceof oracle.jdbc.OracleTypeMetaData.Struct) {
+        oracle.jdbc.OracleTypeMetaData.Struct structMetadata = (oracle.jdbc.OracleTypeMetaData.Struct) oracleMetaData;
+        String[] subtypeNames = structMetadata.getSubtypeNames();
+        System.out.println(subtypeNames);
+        ResultSetMetaData metaData = structMetadata.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        for (int i = 0; i < columnCount; i++) {
+          System.out.println(metaData.getColumnName(i + 1));
+        }
+      }
     }
     return new ORADataImpl(datum);
   }
